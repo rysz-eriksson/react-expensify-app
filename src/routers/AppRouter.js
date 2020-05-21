954818ef84634e91c8b8283d46b7384797bb80ea
rysz-eriksson <ryszard.wolosiuk@gmail.com>
2020-05-21 09:55:01 +0200

    fix: update routes

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import ExpenseDashboardPage from '../components/ExpenseDashboard';
-import AddExpensePage from '../components/AddExpense';
-import EditExpensePage from '../components/EditExpense';
-import HelpPage from '../components/Help';
-import NotFoundPage from '../components/NotFound';
+import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
+import AddExpensePage from '../components/AddExpensePage';
+import EditExpensePage from '../components/EditExpensePage';
+import HelpPage from '../components/HelpPage';
+import NotFoundPage from '../components/NotFoundPage';
 import Header from '../components/Header';
 
   const AppRouter = () => (
